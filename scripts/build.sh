18624f355d94924ea6aaaecadbfeb9e9090c4738
Ringo Hoffmann <riho@live.de>
2019-02-01 10:40:17 +0100

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,29 @@ OS=linux
 ARCH=amd64
 #######################
 
+[ -z $1 ] || { 
+    OS=$1
+}
+
+[ -z $2 ] || {
+    OS=$2
+}
+
 DATE=$(date -u '+%Y-%m-%d_%I:%M:%S%p')
 TAG=$(git describe --tags)
 COMMIT=$(git rev-parse HEAD)
 
-(env GOOS=$OS GOARCH=$ARCH go build -v -o tokenTools -ldflags "-X main.appDate=$DATE -X main.appVersion=$TAG -X main.appCommit=$COMMIT")
+BIN=./bin/tokenTools
+
+[ "$OS" == "windows" ] && {
+    BIN=${BIN}.exe
+}
+
+(
+    env GOOS=$OS GOARCH=$ARCH \
+        go build \
+            -v \
+            -o ${BIN} \
+            -ldflags "-X main.appDate=$DATE -X main.appVersion=$TAG -X main.appCommit=$COMMIT" \
+            ./cmd/tokentools
+)
