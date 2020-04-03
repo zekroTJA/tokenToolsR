b0162563c863782e59c377f04591c7db69a0b3e4
zekroTJA <riho@live.de>
2020-04-03 19:44:19 +0200

    add logos and icons

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -2,37 +2,37 @@
 
 import React, { Component } from 'react';
 import RestAPI from '../../api/rest';
-import { ReactComponent as Logo } from '../../img/header-logo.svg';
 import { Version } from '../../api/model';
 import { Link } from 'react-router-dom';
 
+import { ReactComponent as Logo } from '../../img/logo-wide.svg';
 import './Header.scss';
 
 export default class Header extends Component {
-  public state = {
-    version: {} as Version,
-  };
+  // public state = {
+  //   version: {} as Version,
+  // };
 
-  public async componentDidMount() {
-    this.setState({
-      version: (await RestAPI.getVersion())?.data,
-    });
-  }
+  // public async componentDidMount() {
+  //   this.setState({
+  //     version: (await RestAPI.getVersion())?.data,
+  //   });
+  // }
 
   public render() {
     return (
       <div className="header">
-        <Link to="/">
-          <Logo />
+        <Link to="/" className="header-logo">
+          <Logo height="25" width="177" />
         </Link>
-        {this.state.version && (
+        {/* {this.state.version && (
           <span className="version-info">{this.version}</span>
-        )}
+        )} */}
       </div>
     );
   }
 
-  private get version(): string {
-    return `build ${this.state.version.version} (${this.state.version.commit}) [${this.state.version.date}]`;
-  }
+  // private get version(): string {
+  //   return `build ${this.state.version.version} (${this.state.version.commit}) [${this.state.version.date}]`;
+  // }
 }
